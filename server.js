72828cc2b39cf0a455707eca14e75ac942164a0b
Rahil Rahimi <rahilrahimi1362@gmail.com>
2021-11-11 15:23:59 -0500

    revised server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -11,7 +13,18 @@ app.use(express.json());
 app.use((req, res) => {
   res.status(404).end();
 });
-
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
